274b00cb655387dac290f49e5188a29c9c0ece02
James Hwang <91juhwang@gmail.com>
2024-02-22 10:49:57 -0500

    Use <Link> component instead of anchor tag

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -52,9 +52,9 @@ export default function Example() {
                     {navigation.map((item) => (
                       <Link
                         key={item.name}
-                        href={item.href}
+                        to={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          '[&.active]:bg-gray-900 [&.active]:text-white text-gray-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
